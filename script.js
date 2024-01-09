06cc8ab9cadb6d8d249d72f3dfed3d16da31e774
Pavlo Karpenko <karpenkopasha19@outlook.com>
2024-01-09 23:31:59 +0200

    Correct keyboard v.01

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function keyPush(event) {
-    let inputKey = document.querySelector('.inputKey');
     let butPress = document.querySelectorAll('.butPress');
 
     if(event.key === 'Enter') {
@@ -12,19 +11,31 @@ function keyPush(event) {
             // Якщо в інпут введені елементи які мають класс butPress 
             if (event.key === item.textContent) {
                 item.classList.add('active');
+            } else if(event.key !== item.textContent) {
+                item.classList.remove('active');
+            }
+        })
+    } else if(event.key === 'Alt') {
+        event.preventDefault();
+        butPress.forEach(function(item) {
+            if (event.key === item.textContent) {
+                item.classList.add('active');
+            } else if(event.key !== item.textContent) {
+                item.classList.remove('active');
             }
         })
     // В else такий же код як і вище
-    // код вище це костиль для кнопки Enter
+    // код вище це костиль для кнопок Enter та Alt
     // тобто він такий же самий як і в else
     // але в ньму є відключення відправки форми
     } else {
         butPress.forEach(function(item) {
             if (event.key === item.textContent) {
                 item.classList.add('active');
+            } else if(event.key !== item.textContent) {
+                item.classList.remove('active');
             }
         })
-        console.log(event);
     }
 }
 
